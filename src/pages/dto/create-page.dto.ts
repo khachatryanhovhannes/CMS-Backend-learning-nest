4795e4bfd00ccb95eae3047d66bf6a8d6f91c908
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-28 11:23:48 +0400

    develop: Installing class validator

--- src/pages/dto/create-page.dto.ts
+++ src/pages/dto/create-page.dto.ts
@@ -1,7 +1,19 @@
+import { IsString, IsNotEmpty, IsObject } from 'class-validator';
+
 export class CreatePageDto {
+  @IsString()
+  @IsNotEmpty()
   readonly title: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly content: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly category: string;
+
+  @IsObject()
   readonly metadata: {
     description: string;
     keywords: string;
