6ce0f71e76757e44f75cc4020fba973b36bc92e4
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-28 11:12:23 +0400

    develop: Adding page module page create functionality

--- src/pages/pages.service.ts
+++ src/pages/pages.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePageDto } from './dto';
 
 @Injectable()
-export class PagesService {}
+export class PagesService {
+  private readonly pages: CreatePageDto[] = [];
+
+  addPage(createPageDto: CreatePageDto) {
+    this.pages.push(createPageDto);
+  }
+}
