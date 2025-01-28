2a124d7a9130252612d7ce7aecfd1a581e0fbcba
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2025-01-28 15:09:15 +0400

    Adding response interceptot for handling response data

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const response = context.switchToHttp().getResponse();
+
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: response.statusCode,
+        success: true,
+        message: 'Request successful',
+        data,
+      })),
+    );
+  }
+}
